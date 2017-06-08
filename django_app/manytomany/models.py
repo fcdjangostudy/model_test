240209f9c2e6cb9aa2394b264013c503a04af3c2
GAIUS8 <gaius827@gmail.com>
2017-06-08 17:31:43 +0900

    intial settinngs and add inital models

--- django_app/manytomany/models.py
+++ django_app/manytomany/models.py
@@ -4,10 +4,15 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Lecture(models.Model):
     title = models.CharField(max_length=100)
-    students = models.ForeignKey(
+    students = models.ManyToManyField(
         'Student',
-        on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.title
